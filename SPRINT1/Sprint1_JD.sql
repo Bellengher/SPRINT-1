e09bfb2331cc5b7135e4a6d751a12ea9a29940a0
JaviDoria <160622695+JaviDoria@users.noreply.github.com>
2024-03-03 21:35:30 +0100

    Update Sprint1_JD.sql

--- SPRINT1/Sprint1_JD.sql
+++ SPRINT1/Sprint1_JD.sql
@@ -57,19 +57,6 @@ HAVING COUNT(id) > 1; 					#3º HAVING para que me diga del COUNT(id) cuales son
  LIMIT 5;								#5º Porque pide las 5 mas costosas
 
 
-#Ejercicio 2 version chunga (Había entendido que tenía que buscar las 5 ventas más altas y luego sumarlas)
-SELECT 
-	(SELECT 
-		(SELECT SUM(amount) FROM 
-			(SELECT amount FROM transaction 
-            ORDER BY amount DESC LIMIT 5) AS Top_cinco)) AS Suma_Top_cinco, timestamp,amount
-FROM transaction
-WHERE declined = 0 
-ORDER BY amount DESC
-LIMIT 5 ;
-
-
-
 #Ejercicio 3 En quin dia es van realitzar les cinc vendes de menor valor? Mostra la data de la transacció i la sumatòria de la quantitat de diners.
 	
  SELECT DATE(timestamp)AS fecha, SUM(amount) AS suma FROM transaction 	#1º DATE(timestamp) para que solo tome la fecha y no la hora,suma amount de transaction 
@@ -78,15 +65,6 @@ LIMIT 5 ;
  ORDER BY suma ASC							#4º Ordeno de manera ascendente la suma
  LIMIT 5;								#5º Porque pide las 5 de menor valor
  
-#Ejercicio 3 version chunga (Había entendido que debía buscar las 5 ventas más bajas y luego sumarlas)
-SELECT
-		(SELECT SUM(amount) FROM 
-			(SELECT amount FROM transaction ORDER BY amount ASC LIMIT 5) AS Flop_cinco) AS Suma_Flop_cinco,timestamp,amount
-FROM transaction
-WHERE declined = 0 
-ORDER BY amount ASC
-LIMIT 5 ;
-
 
 #Ejercicio 4  Quina és la mitjana de despesa per país? Presenta els resultats ordenats de major a menor mitjà.
 	
